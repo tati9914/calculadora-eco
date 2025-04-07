d819e2b6a0cf9d8697c2f5d383e201ac8bf8b2d2
tati9914 <tatikononovas@gmail.com>
2025-04-07 20:55:09 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+<li class="list__item">
+        <a href={{size + "/1"}}>
+          <img class="item__img" src="https://cdn-icons-png.flaticon.com/512/6849/6849015.png" alt="light">
+          <span>nuestra opcion de bajo consumo</span></a>
+      </li>
